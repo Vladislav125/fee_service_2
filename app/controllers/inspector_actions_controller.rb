26f7bcf2912e21ab5054db435ddbb6b0676460ea
Vladislav <myasoedovvladislav125@yandex.ru>
2023-11-20 23:15:12 +1000

    Income adding created.

--- app/controllers/inspector_actions_controller.rb
+++ app/controllers/inspector_actions_controller.rb
@@ -1,9 +1,24 @@
 class InspectorActionsController < ApplicationController
-  before_action :inspector_or_admin_user, only: [:index, :show_user]
+  before_action :inspector_or_admin_user, only: [:index, :show_user, :edit_user, :update_user]
 
   def index
     @users = User.order(:surname).page params[:page]
     @user = current_user
+  end
+
+  def users_list
+    @users = User.order(:surname).page params[:page]
+    render turbo_stream: turbo_stream.update('index', partial: 'users')
+  end
+
+  def estates_list
+    @estates = Estate.order(:address).page params[:page]
+    render turbo_stream: turbo_stream.update('index', partial: 'estates')
+  end
+
+  def vehicles_list
+    @vehicles = Vehicle.order(:state_number).page params[:page]
+    render turbo_stream: turbo_stream.update('index', partial: 'vehicles')
   end
 
   def show_user
@@ -13,12 +28,27 @@ class InspectorActionsController < ApplicationController
   end
 
   def edit_user
-    
+    @user = User.find(params[:id])
+  end
+
+  def update_user
+    @user = User.find(params[:id])
+    if @user.update_column(:income, params[:income])
+      flash[:success] = "Данные профиля обновлены."
+      redirect_to show_user_path(@user.id)
+    else
+      @errors = @user.errors.map(&:message)
+      render turbo_stream: turbo_stream.update('errors', partial: 'errors')    
+    end
   end
 
   private
 
     def inspector_or_admin_user
       redirect_to(root_path) unless current_user.inspector? || current_user.admin?
+    end
+
+    def user_params_for_edit
+      params.require(:user).permit(:income)
     end
 end
--- app/controllers/personal_actions_controller.rb
+++ app/controllers/personal_actions_controller.rb
@@ -1,15 +1,28 @@
 class PersonalActionsController < ApplicationController
-  before_action :check_log_in, only: %i[account]
+  before_action :logged_in_user, only: [:account]
 
   def account
     @user = current_user
   end
 
+  def become_entrepreneur
+  end
+
+  def confirm_status
+  end
+
   private
 
-    def check_log_in
+    def logged_in_user
       unless logged_in?
-        redirect_to root_url
+        store_location
+        flash[:danger] = "Пожалуйста, авторизуйтесь."
+        redirect_to login_path
       end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end
