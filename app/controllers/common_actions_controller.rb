27d8dca2ef477c5fb3ab18ac47b92c2706568007
Vladislav <myasoedovvladislav125@yandex.ru>
2023-11-21 06:44:43 +1000

    Clear busines function added.

--- app/controllers/common_actions_controller.rb
+++ app/controllers/common_actions_controller.rb
@@ -1,15 +1,20 @@
 class CommonActionsController < ApplicationController
   def clear_business
-
   end
 
   def find_business
-    @identificator = params[:identificator]
-    @user = User.find_by(inn: @identificator)
-    if @user != nil && @user.ipid != nil
-      render turbo_stream: turbo_stream.update('result', partial: 'result_inn')
+    identificator = params[:identificator]
+    @result = User.find_by(inn: identificator)
+      
+    if @result != nil && @user.ipid != nil
+      render turbo_stream: turbo_stream.update('result', partial: 'entrepreneur')
     else
-      render turbo_stream: turbo_stream.update('result', partial: 'not_found')
+      @result = Organization.find_by(org_inn: identificator)
+      if @result != nil
+        render turbo_stream: turbo_stream.update('result', partial: 'organization')
+      else
+        render turbo_stream: turbo_stream.update('result', partial: 'not_found')
+      end
     end
     # render turbo_stream: turbo_stream.update('result_inn', partial: 'result_inn')    
 
